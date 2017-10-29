c7201fbf5dc2fb318c1cf4d28487d9f8eca909e5
samuelkadolph <samuel@kadolph.com>
2017-10-29 02:57:02 -0400

    Playing around

--- devicetypes/z-rowenta/rowenta-fan.src/rowenta-fan.groovy
+++ devicetypes/z-rowenta/rowenta-fan.src/rowenta-fan.groovy
@@ -2,41 +2,40 @@
  *  Rowenta Fan
  *
  *  Copyright 2017 Samuel Kadolph
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
  */
 metadata {
-	definition (name: "Rowenta Fan", namespace: "z-rowenta", author: "Samuel Kadolph") {
-		capability "Switch Level"
-	}
+  definition (name: "Rowenta Fan", namespace: "z-rowenta", author: "samuelkadolph") {
+    capability "Switch Level"
+  }
 
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+  simulator {
+  }
 
-	tiles {
-		// TODO: define your main and details tiles here
-	}
+  tiles {
+    standardTile("switch", "device.switch", width: 2, height: 2) {
+      state "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
+      state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#00A0DC", nextState:"turningOff"
+      state "turningOn", label:'${name}', icon:"st.switches.switch.on", backgroundColor:"#00A0DC"
+      state "turningOff", label:'${name}', icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+    }
+    controlTile("levelSliderControl", "device.level", "slider", height: 2, width: 1, inactiveLabel: false) {
+      state "level", action:"setLevel"
+    }
+    standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+      state "default", label:"", action:"refresh", icon:"st.secondary.refresh"
+    }
+
+    main "switch"
+    details "switch", "levelSliderControl", "refresh"
+  }
 }
 
-// parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
-	// TODO: handle 'level' attribute
+  log.debug("Parsing '${description}'")
 
 }
 
-// handle commands
 def setLevel() {
-	log.debug "Executing 'setLevel'"
-	// TODO: handle 'setLevel' command
+  log.debug("Executing 'setLevel'")
 }
