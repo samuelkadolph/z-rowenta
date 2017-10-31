04e02abde8488a913cccff5f59f8cbe30df11d5c
samuelkadolph <samuel@kadolph.com>
2017-10-31 18:50:03 -0400

    Working on the z-uno code

--- z-rowenta.ino
+++ z-rowenta.ino
@@ -2,26 +2,35 @@
 #define MEDIUM_SPEED_PIN 6
 #define HIGH_SPEED_PIN 7
 #define BOOST_SPEED_PIN 8
-#define BOOST_BUTTON_PIN 9
-#define ONOFF_BUTTON_PIN 10
-#define SPEED_BUTTON_PIN 11
+#define BOOST_BUTTON_PIN 25
+#define ONOFF_BUTTON_PIN 2
+#define SPEED_BUTTON_PIN 24
 
+// Fan Power Values
 #define ON 1
 #define OFF 0
 
+// Fan Speed Values
 #define LOW_SPEED 1
 #define MEDIUM_SPEED 2
 #define HIGH_SPEED 3
 #define BOOST_SPEED 4
 
+// State Change Flags
 #define POWER_CHANGED 1
 #define SPEED_CHANGED 2
+
+// Z-Wave Channels
 #define POWER_CHANNEL 1
 #define SPEED_CHANNEL 2
 
 ZUNO_SETUP_SLEEPING_MODE(ZUNO_SLEEPING_MODE_ALWAYS_AWAKE);
 ZUNO_SETUP_CHANNELS(ZUNO_SWITCH_BINARY(getPower, setPower), ZUNO_SWITCH_MULTILEVEL(getSpeed, setSpeed));
 
+// Fan Microcontroller Constants
+#define MICRO_WAIT 25
+
+// Global State
 byte powerValue = 0;
 byte speedValue = 0;
 byte targetPower = 0;
@@ -30,23 +39,22 @@ boolean changePower = false;
 boolean changeSpeed = false;
 
 void setup() {
-  pinMode(LOW_SPEED_PIN, INPUT);
-  pinMode(MEDIUM_SPEED_PIN, INPUT);
-  pinMode(HIGH_SPEED_PIN, INPUT);
-  pinMode(BOOST_SPEED_PIN, INPUT);
+  pinMode(LOW_SPEED_PIN, INPUT); // INPUT_PULLUP does not work with Pin 5 -- https://forum.z-wave.me/viewtopic.php?f=3427&t=25688
+  pinMode(MEDIUM_SPEED_PIN, INPUT_PULLUP);
+  pinMode(HIGH_SPEED_PIN, INPUT_PULLUP);
+  pinMode(BOOST_SPEED_PIN, INPUT_PULLUP);
 
   pinMode(BOOST_BUTTON_PIN, OUTPUT);
   pinMode(ONOFF_BUTTON_PIN, OUTPUT);
   pinMode(SPEED_BUTTON_PIN, OUTPUT);
 
-  digitalWrite(ONOFF_BUTTON_PIN, LOW);
-
   pinMode(13, OUTPUT);
 
   Serial.begin();
 
-//  readState();
-//  sendReport();
+  readState();
+  zunoSendReport(POWER_CHANNEL);
+  zunoSendReport(SPEED_CHANNEL);
 }
 
 void loop() {
@@ -89,10 +97,35 @@ void loop() {
   if (changePower) {
     if (targetPower != powerValue) {
       digitalWrite(ONOFF_BUTTON_PIN, HIGH);
-    } else {
+      delay(MICRO_WAIT);
       digitalWrite(ONOFF_BUTTON_PIN, LOW);
-      changePower = false;
     }
+
+    changePower = false;
+  }
+
+  if (changeSpeed) {
+    if (targetSpeed != speedValue) {
+      if (targetSpeed == BOOST_SPEED || speedValue == BOOST_SPEED) {
+        digitalWrite(BOOST_BUTTON_PIN, HIGH);
+        delay(MICRO_WAIT);
+        digitalWrite(BOOST_BUTTON_PIN, LOW);
+        delay(MICRO_WAIT);
+        readState();
+      }
+
+      while (targetSpeed != speedValue) {
+        digitalWrite(SPEED_BUTTON_PIN, HIGH);
+        delay(MICRO_WAIT);
+        digitalWrite(SPEED_BUTTON_PIN, LOW);
+        delay(MICRO_WAIT);
+        readState();
+      }
+      
+      zunoSendReport(SPEED_CHANNEL);
+    }
+
+    changeSpeed = false;
   }
 
   delay(50);
@@ -146,6 +179,7 @@ void setPower(byte value) {
   Serial.print("setPower(");
   Serial.print(value);
   Serial.println(")");
+
   changePower = true;
   targetPower = value == 0 ? OFF : ON;
 }
@@ -154,6 +188,7 @@ void setSpeed(byte value) {
   Serial.print("setSpeed(");
   Serial.print(value);
   Serial.println(")");
+
   changeSpeed = true;
   targetSpeed = value;
 }
